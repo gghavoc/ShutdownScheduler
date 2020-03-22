7a59b098d3c4fcd63223023d2f44dea05ab54b85
Clarence Manuel <clarencemanuel112@outlook.com>
2020-03-22 09:04:45 +0800

    Updated CMakeList.txt

---
+++ cmake-build-debug/CMakeFiles/ShutdownScheduler.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/clarence/vcsrepo/CLion/ShutdownScheduler/main.cpp" "/home/clarence/vcsrepo/CLion/ShutdownScheduler/cmake-build-debug/CMakeFiles/ShutdownScheduler.dir/main.cpp.o"
+  "/home/clarence/vcsrepo/CLion/ShutdownScheduler/src/MainApp.cpp" "/home/clarence/vcsrepo/CLion/ShutdownScheduler/cmake-build-debug/CMakeFiles/ShutdownScheduler.dir/src/MainApp.cpp.o"
+  "/home/clarence/vcsrepo/CLion/ShutdownScheduler/src/MainFrame.cpp" "/home/clarence/vcsrepo/CLion/ShutdownScheduler/cmake-build-debug/CMakeFiles/ShutdownScheduler.dir/src/MainFrame.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx-3.1.3-gtkudml/lib/wx/include/gtk3-unicode-static-3.1"
+  "/usr/local/lib/wx-3.1.3-gtkudml/include/wx-3.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ShutdownScheduler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShutdownScheduler.dir/main.cpp.o"
+  "CMakeFiles/ShutdownScheduler.dir/src/MainApp.cpp.o"
+  "CMakeFiles/ShutdownScheduler.dir/src/MainFrame.cpp.o"
+  "ShutdownScheduler"
+  "ShutdownScheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShutdownScheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
